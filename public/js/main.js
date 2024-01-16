772a04e73e17f8579fa1ec7c3f297eaa3d045a1f
SMH-Software <77654418+SMH-Software@users.noreply.github.com>
2024-01-16 19:14:39 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -42,11 +42,11 @@ const getAccounts = async (endpoint) => {
 
 
 //Get all Accounts
-getAccounts('https://server-murex-five.vercel.app/api/accounts-register')
+getAccounts('https://server-kco4.vercel.app/api/accounts-register')
 
 
 //Add a new account 
-addAccount('https://server-murex-five.vercel.app/api/accounts-register', form, formInputs, message)
+addAccount('https://server-kco4.vercel.app/api/accounts-register', form, formInputs, message)
 
 //Update account 
 updateAccount(formUpdate, formInputsUpdate, messageSuccess)
